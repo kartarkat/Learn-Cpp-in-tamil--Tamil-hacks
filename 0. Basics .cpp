817bd7c1af2e74e5a5e6de9854aadc81a41b4864
kartar kat <kartarkat@gmail.com>
2019-07-29 23:44:23 +0530

    Add files via upload

---
+++ 0. Basics .cpp
@@ -0,0 +1,58 @@
+
+/*
+author : karthikeyan 
+year and month : 2019 , july
+Learn c++ in tamil from Tamil Hacks - You Tube channel
+Tutorial links : 
+	Learn c++ in tamil : http://bit.ly/2Mpvqj8
+	Learn c   in tamil : http://bit.ly/2ZjDB43
+
+c++ program to find out the number is even or odd
+
+*/
+
+#include <iostream>     //header file or library file
+using namespace std;	//standard namespace declaration
+
+int main()			//main funtion - program starts here
+{
+	
+	cout<<"hi guys welcome to Tamil hacks"<<"\n";
+	
+	// are used for single line comments
+	/* are used for ultiple line comments*/
+
+	int a;  		// variable declartion
+	a = 20; 		//variable assignment
+	int b = 10 ; 	// variable declartion and initialization
+	
+	// "\n" for new line
+	
+	cout<<"hi "<<"\n";     //basic output statement
+	cout<<a+b<<"\n";	   //basic addition operation
+	cout<<a-b<<"\n";	   //basic subtract operation
+	
+	int c = 0 ;    //assiging 0 to avoid grabage value
+	cout<<"enter c value ";
+	cin>>c;  	   //basic input statemnet
+	
+	c = a+b+a;	   //assignment with additon operation
+	
+	cout<<"value of c is "<<c<<"\n";
+	//print values and exact strings in double quotes
+
+	
+	return 0;	  //main function ends here 
+
+}				  //program terminates here
+
+/*
+Hi FRIENDS, Welcome to my page , i am an technician , who loves to code  , 
+do videos, make apps, websites, learn something new every day.
+
+my youtube channel link : https://www.youtube.com/tamilhacks
+
+learn all new technology and programming here in my channel in tamil easily.
+Thank you Guys - with love from Tamil Hacks
+contact me at tamilhackstech@gmail.com
+*/
